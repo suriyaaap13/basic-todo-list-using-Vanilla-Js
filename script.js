b1268ddfaa34573b28f7fd9503490b80eb8e9940
Suriyaa <suriyaaap13@gmail.com>
2021-08-23 21:31:14 +0530

    create the text-box html and css

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log('entered js');
+var inputBox = document.getElementById("input-box");
+var btn = document.getElementById('submit-btn');
+
+
+inputBox.addEventListener('keydown',function(){
+    console.log('I am in');
+    var text = inputBox.value;
+    console.log(text);
+    console.log(text.length);
+    if(text.length==0){
+        btn.style.display = "none";
+        return;
+    }
+    if(text.length>0){
+        console.log('show btn');
+        btn.style.display = "flex";
+        return;
+    }
+})
